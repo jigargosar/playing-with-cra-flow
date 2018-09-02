0c01ffdfe89dc0cec2e6a6b2135da06c1c63892b
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-02 13:37:02 +0530

    WIP basic styling

--- src/elements/Output.js
+++ src/elements/Output.js
@@ -1,5 +1,9 @@
 import {Base, styled} from 'reakit'
 
+export const OutputWrapper = styled(Base)`
+  margin: 0 1rem;
+`
+
 export const Output = styled(Base)`
   font-family: monospace;
   font-size: 1.5rem;
