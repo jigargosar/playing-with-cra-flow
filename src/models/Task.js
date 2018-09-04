38957d84402f2eb245204f09ba50eef5e9030300
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-04 09:31:31 +0530

    WIP flow

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,8 +1,17 @@
 // @flow
+
 import * as faker from 'faker'
+import type { Category } from './Category'
 import { categories } from './Category'
 
-export function createTask() {
+type Task = {
+  category: Category,
+  done: boolean,
+  id: string,
+  title: string,
+}
+
+export function createTask(): Task {
   return {
     id: faker.random.alphaNumeric(4),
     title: faker.random.words(),
