a82fa0a65c687bcc82779a65739fda4fe9fbb780
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-06 14:36:39 +0530

    WIP tag

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,16 +1,22 @@
 // @flow
 
 export type Category =
+  | 'Calendar'
   | 'InBasket'
   | 'NextAction'
   | 'Project'
   | 'Someday'
+  | 'Reference'
   | 'WaitingFor'
+  | 'Incubator'
 
 export const categories: Array<Category> = [
   'InBasket',
+  'Calendar',
   'WaitingFor',
   'NextAction',
   'Project',
+  'Incubator',
   'Someday',
+  'Reference',
 ]
