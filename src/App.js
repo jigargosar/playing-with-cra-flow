45e412147921e7ccf31e0ec29b72f191cc41e071
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-07 16:01:28 +0530

    WIP re-start: TaskItem

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ import { Icon } from 'react-icons-kit'
 import { home } from 'react-icons-kit/icomoon/home'
 import { chevronDown } from 'react-icons-kit/feather'
 import type { TaskCollection } from './models/Task'
-import { generateTaskList } from './models/Task'
+import { generateTaskList, setSomeTaskTags } from './models/Task'
 import { TaskList } from './components/TaskList'
 import { theme } from './components/theme'
 import { Sidebar } from './components/Sidebar'
 import { AppLayout } from './components/AppLayout'
+import type { TagCollection } from './models/Tag'
 import { generateTagList } from './models/Tag'
 
 export const IconHome = () => <Icon size={'100%'} icon={home} />
 export const ChevronDown = () => <Icon size={'100%'} icon={chevronDown} />
 
-type AppState = { tasks: TaskCollection }
+type AppState = { tasks: TaskCollection, tags: TagCollection }
 
 class App extends Component<{}, AppState> {
-  state: AppState = { tasks: [] }
+  state: AppState = { tasks: [], tags: [] }
 
   componentDidMount() {
     const tags = generateTagList()
-    const tasks = generateTaskList()
-    this.setState({ tasks: tasks })
+    const tasks = generateTaskList().map(setSomeTaskTags(tags))
+    this.setState({ tasks, tags })
   }
 
   render() {
