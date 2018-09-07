09949bd002a6a2cb4505c249099abf9096e51a73
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-07 10:29:13 +0530

    WIP re-start

--- src/components/TaskList.fixtures.js
+++ src/components/TaskList.fixtures.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import { TaskList } from './TaskList'
 import { createTaskList } from '../models/Task'
+import { theme } from './theme'
 
 export default [
   {
     component: TaskList,
     props: {
+      theme,
       tasks: createTaskList(),
     },
   },
