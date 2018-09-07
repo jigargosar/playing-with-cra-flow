5436f83902d93abfe1df7543017eb3a5b805c947
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-07 18:27:42 +0530

    WIP @reach/router

---
+++ src/components/RouterLink.js
@@ -0,0 +1,44 @@
+// @flow
+
+import * as React from 'react'
+import { styled } from 'reakit'
+import { Link as ReachRouterLink } from '@reach/router'
+import cn from 'classnames'
+
+type Props = {}
+
+export function RouterLink({ ...other }: Props) {
+  return (
+    <RouterLinkWrapper>
+      <ReachRouterLink getProps={propsToCN} {...other} />
+    </RouterLinkWrapper>
+  )
+}
+
+function propsToCN({ isCurrent, isPartiallyCurrent }) {
+  return {
+    className: cn({
+      current: isCurrent,
+      partiallyCurrent: isPartiallyCurrent,
+    }),
+  }
+}
+
+const RouterLinkWrapper = styled.span`
+  > a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  > a.partiallyCurrent {
+    color: rgba(255, 99, 71, 0.3);
+  }
+
+  > a.current {
+    color: rgba(255, 99, 71, 1);
+  }
+
+  > a:hover {
+    text-decoration: underline;
+  }
+`
