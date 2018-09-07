275015d7d524cdb680d0f39bac7d789c0a09ca07
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-07 15:03:56 +0530

    WIP re-start: TaskList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,26 +1,23 @@
 // @flow
 
 import * as React from 'react'
-import { Component } from 'react'
 import type { Task } from '../models/Task'
 
 type Props = {
   tasks: Task[],
 }
 
-export class TaskList extends Component<Props> {
-  render() {
-    return (
-      <div>
-        <h1>TaskList</h1>
-        {this.props.tasks.map(task => {
-          return (
-            <div key={task.id}>
-              <div>{task.title}</div>
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
+export function TaskList({ tasks }: Props) {
+  return (
+    <div>
+      <h1>TaskList</h1>
+      {tasks.map(task => {
+        return (
+          <div key={task.id}>
+            <div>{task.title}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
