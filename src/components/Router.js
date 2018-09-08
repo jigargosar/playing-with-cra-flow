bed87fc2b7460ab1ed92cfa5a685f022c0e304f2
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-08 16:49:34 +0530

    WIP @reach/component-component

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,45 +1,8 @@
-import cn from 'classnames'
 import { Link as ReachRouterLink } from '@reach/router'
 import * as React from 'react'
 import { styled } from 'reakit'
 
 export const Route = ({ render, ...otherProps }) => render(otherProps)
-
-export function LinkTo2({ ...other }) {
-  return (
-    <RouterLinkWrapper>
-      <ReachRouterLink getProps={propsToCN} {...other} />
-    </RouterLinkWrapper>
-  )
-}
-
-function propsToCN({ isCurrent, isPartiallyCurrent }) {
-  return {
-    className: cn({
-      current: isCurrent,
-      partiallyCurrent: isPartiallyCurrent,
-    }),
-  }
-}
-
-const RouterLinkWrapper = styled.span`
-  > a {
-    color: inherit;
-    text-decoration: none;
-  }
-
-  > a.partiallyCurrent {
-    color: rgba(255, 99, 71, 0.8);
-  }
-
-  > a.current {
-    color: rgba(255, 99, 71, 1);
-  }
-
-  > a:hover {
-    text-decoration: underline;
-  }
-`
 
 export const LinkTo = styled(ReachRouterLink)`
   color: inherit;
@@ -55,5 +18,7 @@ export const LinkTo = styled(ReachRouterLink)`
 `
 
 LinkTo.defaultProps = {
-  getProps: ({ isCurrent }) => ({ 'data-is-current': isCurrent }),
+  getProps: ({ isCurrent, location }) => ({
+    'data-is-current': isCurrent,
+  }),
 }
