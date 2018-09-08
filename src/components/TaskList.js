93686218444d2dcd90f2bf8f66f308f7cb249ec7
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-08 19:45:53 +0530

    WIP @reach

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import { Fragment } from 'react'
 import type { Task as TaskModel } from '../models/Task'
-import { Divider, styled } from 'reakit'
+import { Divider } from 'reakit'
 import { TaskItem } from './Task'
 
 type Props = {
@@ -11,24 +11,16 @@ type Props = {
   title: string,
 }
 
-export const Tasks = styled.div`
-  > * {
-    margin: 1rem 0;
-  }
-`
-
 export function TaskList({ tasks, title }: Props) {
   return (
     <Fragment>
       <h2>{title}</h2>
-      <Tasks>
-        {tasks.map(task => (
-          <Fragment key={task.id}>
-            <TaskItem task={task} />
-            <Divider />
-          </Fragment>
-        ))}
-      </Tasks>
+      {tasks.map(task => (
+        <Fragment key={task.id}>
+          <TaskItem task={task} />
+          <Divider />
+        </Fragment>
+      ))}
     </Fragment>
   )
 }
