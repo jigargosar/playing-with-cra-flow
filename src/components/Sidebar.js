846e41cdbab19d08e51bdedd8ea7f87a0eaf51c0
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-11 17:25:05 +0530

    replace emotion with typestyle

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,10 @@
 
 import * as React from 'react'
 import { Component } from 'react'
-import styled from 'react-emotion'
 import { categories } from '../models/Category'
 import { LinkTo } from './Router'
 import { LinkToCategory } from './Links'
-import { rem, style, verticallySpaced } from './typestyle'
+import { padding, rem, style, verticallySpaced } from './typestyle'
 
 type Props = {}
 
@@ -27,15 +26,7 @@ export class Sidebar extends Component<Props> {
   }
 }
 
-const Layout = styled.div`
-  > * {
-    display: block;
-    margin: 0.5rem 1.5rem;
-    text-transform: uppercase;
-  }
-`
-
-const containerStyle = style(verticallySpaced(rem(1)), {
+const containerStyle = style(padding(rem(1)), verticallySpaced(rem(0.5)), {
   $nest: {
     '> a': { display: 'block' },
   },
