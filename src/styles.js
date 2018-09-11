e77326a7f063ee640858ba48221bd6b23fd373b0
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-11 21:17:07 +0530

    styling

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
-import { BoxUnit, padding } from 'csstips'
-import { stylesheet } from 'typestyle'
+import { BoxUnit, normalize, padding, setupPage } from 'csstips'
+import { cssRaw, cssRule, stylesheet } from 'typestyle'
 
 export const ttu = { textTransform: 'uppercase' }
 export const strike = { textDecoration: 'line-through' }
@@ -20,3 +20,75 @@ export const mono = {
   fontFamily: `Menlo, Monaco, Consolas, 'Courier New', monospace`,
 }
 export const css = stylesheet({ antialiased })
+
+export function setupGlobalStyles() {
+  normalize()
+  setupPage('#root')
+
+  cssRule('html,body', antialiased, sans)
+
+  // language=LESS
+  cssRaw`
+    *, :after, :before {
+      border: 0 solid #dae1e7;
+    }
+
+    html,
+    body {
+      margin: 0;
+      padding: 0;
+      font-size: 16px;
+      line-height: 1.5em;
+    }
+
+    button {
+      //font-size: 12px;
+      //min-width: 3rem;
+      //min-height: 1.5rem;
+      text-align: center;
+      font-family: inherit;
+      //text-transform: uppercase;
+      font-weight: 700;
+      //background: transparent;
+      background-color: #3490dc;
+      color: #fff;
+      padding: 0.5rem 1rem;
+      border-radius: .25rem;
+      cursor: pointer;
+      font-size: 100%;
+      line-height: 1.15;
+    }
+
+    button:hover {
+      background-color: #2779bd;
+    }
+
+    input[type='text'] {
+      font-size: 16px;
+      min-height: 1.5rem;
+      font-family: inherit;
+    }
+
+    form {
+      margin-bottom: 1rem;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      margin-top: 0;
+    }
+
+    a {
+      display: inline-block;
+      color: inherit;
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: underline;
+    }
+
+    span {
+      display: inline-block;
+    }
+  `
+}
