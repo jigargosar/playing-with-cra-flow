c5acd0af73dc4cb422d46c892263fd16135e4035
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-11 19:12:43 +0530

    replace emotion with typestyle

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,4 @@
 import { Link as RouterLink, Router as ReachRouter } from '@reach/router'
-import styled from 'react-emotion'
 import * as React from 'react'
 import { classes, style } from './typestyle'
 
@@ -7,29 +6,13 @@ export const Route = ({ render, ...otherProps }) => render(otherProps)
 
 export const Router = ReachRouter
 
-export const Link = styled(RouterLink)`
-  color: inherit;
-  text-decoration: none;
-
-  &[data-is-current='true'] {
-    color: rgba(255, 99, 71, 1);
-  }
-
-  :hover {
-    text-decoration: underline;
-  }
-`
-
-Link.defaultProps = {
-  getProps: ({ isCurrent, location }) => ({
-    'data-is-current': isCurrent,
-  }),
-}
-
 export const LinkTo = ({ className, ...otherProps }) => (
   <RouterLink
     getProps={({ isCurrent, location }) => ({
-      className: classes(style(), className),
+      className: classes(
+        style(isCurrent && { color: 'rgba(255, 99, 71, 1)' }),
+        className,
+      ),
     })}
     {...otherProps}
   />
