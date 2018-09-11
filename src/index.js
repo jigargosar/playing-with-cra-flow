d6bd3f99b167a99a8cb16348254688f4df897f7d
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-11 21:16:53 +0530

    styling

--- src/index.js
+++ src/index.js
@@ -5,75 +5,79 @@ import registerServiceWorker from './registerServiceWorker'
 import { cssRaw, cssRule, normalize, setupPage } from './typestyle'
 import { antialiased, sans } from './styles'
 
-normalize()
-setupPage('#root')
+function setupGlobalStyles() {
+  normalize()
+  setupPage('#root')
 
-cssRule('html,body', antialiased, sans)
+  cssRule('html,body', antialiased, sans)
 
-// language=LESS
-cssRaw`
-  *, :after, :before {
-    border: 0 solid #dae1e7;
-  }
+  // language=LESS
+  cssRaw`
+    *, :after, :before {
+      border: 0 solid #dae1e7;
+    }
 
-  html,
-  body {
-    margin: 0;
-    padding: 0;
-    font-size: 16px;
-    line-height: 1.5em;
-  }
+    html,
+    body {
+      margin: 0;
+      padding: 0;
+      font-size: 16px;
+      line-height: 1.5em;
+    }
 
-  button {
-    //font-size: 12px;
-    //min-width: 3rem;
-    //min-height: 1.5rem;
-    text-align: center;
-    font-family: inherit;
-    //text-transform: uppercase;
-    font-weight: 700;
-    //background: transparent;
-    background-color: #3490dc;
-    color: #fff;
-    padding: 0.5rem 1rem;
-    border-radius: .25rem;
-    cursor: pointer;
-    font-size: 100%;
-    line-height: 1.15;
-  }
+    button {
+      //font-size: 12px;
+      //min-width: 3rem;
+      //min-height: 1.5rem;
+      text-align: center;
+      font-family: inherit;
+      //text-transform: uppercase;
+      font-weight: 700;
+      //background: transparent;
+      background-color: #3490dc;
+      color: #fff;
+      padding: 0.5rem 1rem;
+      border-radius: .25rem;
+      cursor: pointer;
+      font-size: 100%;
+      line-height: 1.15;
+    }
 
-  button:hover {
-    background-color: #2779bd;
-  }
+    button:hover {
+      background-color: #2779bd;
+    }
 
-  input[type='text'] {
-    font-size: 16px;
-    min-height: 1.5rem;
-    font-family: inherit;
-  }
+    input[type='text'] {
+      font-size: 16px;
+      min-height: 1.5rem;
+      font-family: inherit;
+    }
 
-  form {
-    margin-bottom: 1rem;
-  }
+    form {
+      margin-bottom: 1rem;
+    }
 
-  h1, h2, h3, h4, h5, h6 {
-    margin-top: 0;
-  }
+    h1, h2, h3, h4, h5, h6 {
+      margin-top: 0;
+    }
 
-  a {
-    display: inline-block;
-    color: inherit;
-    text-decoration: none;
-  }
+    a {
+      display: inline-block;
+      color: inherit;
+      text-decoration: none;
+    }
 
-  a:hover {
-    text-decoration: underline;
-  }
+    a:hover {
+      text-decoration: underline;
+    }
 
-  span {
-    display: inline-block;
-  }
-`
+    span {
+      display: inline-block;
+    }
+  `
+}
+
+setupGlobalStyles()
 
 function render() {
   ReactDOM.render(<App />, document.getElementById('root'))
