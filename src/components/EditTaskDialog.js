82dd32ade174268e4c7c68cbe696b36e4a51b956
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-12 16:30:20 +0530

    styling

--- src/components/EditTaskDialog.js
+++ src/components/EditTaskDialog.js
@@ -54,7 +54,40 @@ export function renderEditTaskDialogTrigger(render) {
               startEditingTask: task => () =>
                 setState({ showDialog: true, task }),
             })}
-            {showDialog && <EditTaskDialog onDismiss={onDismiss} task={task} />}
+            {showDialog && (
+              <Component getRefs={() => ({ title: React.createRef() })}>
+                {({ refs }) => (
+                  <Dialog
+                    className={style(verticallySpaced(rem(1)))}
+                    onDismiss={onDismiss}
+                  >
+                    <h2>Edit Task</h2>
+                    <div className={style(vertical)}>
+                      <input
+                        ref={refs.title}
+                        type={'text'}
+                        defaultValue={task.title}
+                      />
+                    </div>
+                    <CollectionConsumer>
+                      {({ updateTask }) => (
+                        <button
+                          onClick={() => {
+                            updateTask(
+                              { title: refs.title.current.value },
+                              task,
+                            )
+                            onDismiss()
+                          }}
+                        >
+                          Ok
+                        </button>
+                      )}
+                    </CollectionConsumer>
+                  </Dialog>
+                )}
+              </Component>
+            )}
           </Fragment>
         )
       }}
