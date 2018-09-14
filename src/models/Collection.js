0f9f3033b13c5811928a2da0a66dc88749e0837f
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-14 16:27:43 +0530

    trying folktale maybe

--- src/models/Collection.js
+++ src/models/Collection.js
@@ -1,6 +1,7 @@
-export type Model = {id:string}
+import Maybe from 'folktale/maybe'
+
+export type Model = { id: string }
 export type Collection<Model> = Model[]
 
 export const findByIdOrUndefined = id => c => c.find(m => m.id === id)
-
-
+export const findById = id => c => Maybe.of(c.find(m => m.id === id))
