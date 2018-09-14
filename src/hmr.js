4a868472cdb73249d684491475a8de8e3b74cfaa
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-14 18:50:14 +0530

    hmr toggle clear console

--- src/hmr.js
+++ src/hmr.js
@@ -1,5 +1,8 @@
 // @flow
 
+import ReactDOM from 'react-dom'
+import React from 'react'
+
 export const hotDispose = (disposer: Function, module: Object) => {
   if (module.hot) {
     module.hot.dispose(disposer)
@@ -8,5 +11,17 @@ export const hotDispose = (disposer: Function, module: Object) => {
 export const hotAcceptSelf = (onError: Function, module: Object) => {
   if (module.hot) {
     module.hot.accept(onError)
+  }
+}
+
+export function renderRoot(Comp: Function) {
+  const elementById = document.getElementById('root')
+  if (elementById) {
+    ReactDOM.render(<Comp />, elementById, () => {
+      console.clear()
+      console.log('Render Complete')
+    })
+  } else {
+    throw new Error('root not found')
   }
 }
