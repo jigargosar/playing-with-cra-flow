b1ab3b63d3c0ce9bc866980de80813152e655b00
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-14 22:05:13 +0530

    cleanup

--- src/clearConsoleOnHMR.js
+++ src/clearConsoleOnHMR.js
@@ -5,5 +5,4 @@ const clearConsoleOnHMR = 'clearConsoleOnHMR'
 export const shouldClearConsoleOnHMR = () => storageGet(clearConsoleOnHMR, true)
 export function toggleClearConsoleOnHMR() {
   storageSet(clearConsoleOnHMR, !Boolean(shouldClearConsoleOnHMR()))
-  // console.log(`clearConsoleOnHMR:`, storageGet(clearOnHMRKey, null))
 }
