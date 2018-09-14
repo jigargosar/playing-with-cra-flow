edb026e106ca5cb1d5d7760d74493bd75ada9668
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-14 16:15:02 +0530

    trying testcafe

---
+++ test1.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe'
+
+fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`
+
+test('My first test', async t => {
+  await t.typeText('#developer-name', 'John Smith').click('#submit-button')
+
+  const articleHeader = await Selector('.result-content').find('h1')
+
+  // Obtain the text of the article header
+  const headerText = await articleHeader.innerText
+  await t.expect(headerText).eql('Thank you, John Smith!')
+})
