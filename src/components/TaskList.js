58a2b7ec6cf5ffab46857c8e2d767dcdc51ba34a
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-14 22:15:27 +0530

    cleanup

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -9,17 +9,15 @@ import type { TaskModel } from '../models/Task'
 type Props = { title: string, tasks: TaskModel[] }
 
 export function TaskList({ title, tasks }: Props) {
+  const titleClass = style({
+    fontSize: rem(1.5),
+    marginBottom: rem(1),
+  })
+  const tasksClass = style(verticallySpaced(rem(1.5)))
   return (
     <div>
-      <div
-        className={style({
-          fontSize: rem(1.5),
-          marginBottom: rem(1),
-        })}
-      >
-        {title}
-      </div>
-      <div className={style(verticallySpaced(rem(1.5)))}>
+      <div className={titleClass}>{title}</div>
+      <div className={tasksClass}>
         {tasks.map(task => (
           <Task key={task.id} task={task} />
         ))}
