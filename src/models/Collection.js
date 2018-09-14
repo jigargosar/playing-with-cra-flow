2637787b6c21299dc1968f56aa0faec6309d9543
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-14 17:36:23 +0530

    trying folktale maybe

--- src/models/Collection.js
+++ src/models/Collection.js
@@ -3,5 +3,4 @@ import { nullableToMaybe } from 'folktale/conversions'
 export type Model = { id: string }
 export type Collection<Model> = Model[]
 
-export const findByIdOrUndefined = id => c => c.find(m => m.id === id)
 export const findById = id => c => nullableToMaybe(c.find(m => m.id === id))
