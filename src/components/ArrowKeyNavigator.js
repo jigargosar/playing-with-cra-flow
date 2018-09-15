c80af38685ac6bb75ffe63db92ca67e46d68f82f
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-15 15:36:25 +0530

    arrow focus

--- src/components/ArrowKeyNavigator.js
+++ src/components/ArrowKeyNavigator.js
@@ -9,29 +9,58 @@ import { isHotkey } from 'is-hotkey'
 
 type Props = { children: Function }
 
-export function ArrowKeyNavigator({ children }: Props) {
-  const didMountOrUpdate = ({
-    refs: { containerRef },
-    state: { idx, totalCount },
-    setState,
-  }) => {
-    const containerEl = nullableToMaybe(
-      ReactDOM.findDOMNode(containerRef.current),
-    )
-    containerEl.map(
-      tap(el => {
-        const elList = el.querySelectorAll(
-          `:scope > [tabindex='0'], :scope > a`,
-        )
-        if (totalCount !== elList.length) {
-          setState({ totalCount: elList.length })
-        }
-        const elToFocus = atIndex(idx, elList)
-        requestAnimationFrame(() => elToFocus.map(invoker(0, 'focus')))
-      }),
-    )
-  }
+const didMountOrUpdate = ({
+  refs: { containerRef },
+  state: { idx, totalCount },
+  setState,
+}) => {
+  const containerEl = nullableToMaybe(
+    ReactDOM.findDOMNode(containerRef.current),
+  )
+  containerEl.map(
+    tap(el => {
+      const elList = el.querySelectorAll(`:scope > [tabindex='0'], :scope > a`)
+      if (totalCount !== elList.length) {
+        setState({ totalCount: elList.length })
+      }
+      const elToFocus = atIndex(idx, elList)
+      requestAnimationFrame(() => elToFocus.map(invoker(0, 'focus')))
+    }),
+  )
+}
 
+function onKeyDown(setState, idx, totalCount) {
+  return e => {
+    const isArrowUp = isHotkey('ArrowUp')
+    const isArrowDown = isHotkey('ArrowDown')
+    const isArrowLeft = isHotkey('ArrowLeft')
+    const isArrowRight = isHotkey('ArrowRight')
+
+    cond([
+      //
+      [isArrowUp, () => setState({ idx: mathMod(idx - 1, totalCount) })],
+      [isArrowDown, () => setState({ idx: mathMod(idx + 1, totalCount) })],
+      [isArrowLeft, () => setState({ idx: mathMod(0, totalCount) })],
+      [
+        isArrowRight,
+        () => setState({ idx: mathMod(totalCount - 1, totalCount) }),
+      ],
+    ])(e)
+
+    const isArrowKey = anyPass([
+      isArrowUp,
+      isArrowDown,
+      isArrowRight,
+      isArrowLeft,
+    ])
+
+    if (isArrowKey(e)) {
+      e.preventDefault()
+    }
+  }
+}
+
+export function ArrowKeyNavigator({ children }: Props) {
   return renderWithComponent(
     {
       initialState: { idx: 0, totalCount: 0 },
@@ -40,36 +69,10 @@ export function ArrowKeyNavigator({ children }: Props) {
       didUpdate: didMountOrUpdate,
     },
     ({ refs: { containerRef }, state: { idx, totalCount }, setState }) => {
-      const onKeyDown = e => {
-        const isArrowUp = isHotkey('ArrowUp')
-        const isArrowDown = isHotkey('ArrowDown')
-        const isArrowLeft = isHotkey('ArrowLeft')
-        const isArrowRight = isHotkey('ArrowRight')
-
-        cond([
-          //
-          [isArrowUp, () => setState({ idx: mathMod(idx - 1, totalCount) })],
-          [isArrowDown, () => setState({ idx: mathMod(idx + 1, totalCount) })],
-          [isArrowLeft, () => setState({ idx: mathMod(0, totalCount) })],
-          [
-            isArrowRight,
-            () => setState({ idx: mathMod(totalCount - 1, totalCount) }),
-          ],
-        ])(e)
-
-        const isArrowKey = anyPass([
-          isArrowUp,
-          isArrowDown,
-          isArrowRight,
-          isArrowLeft,
-        ])
-
-        if (isArrowKey(e)) {
-          e.preventDefault()
-        }
-      }
-
-      return children({ containerRef, onKeyDown })
+      return children({
+        containerRef,
+        onKeyDown: onKeyDown(setState, idx, totalCount),
+      })
     },
   )
 }
