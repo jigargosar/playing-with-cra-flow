0759d19710012fdee0b6b6dad283c57a7de08bb2
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-16 15:55:40 +0530

    refactoring: edit task dialog: using react values instead of context

--- src/components/EditTaskDialog.js
+++ src/components/EditTaskDialog.js
@@ -1,12 +1,6 @@
 import * as React from 'react'
 import { Fragment } from 'react'
-import { Dialog } from '@reach/dialog/'
-import { renderWithCollections } from './CollectionContext'
-import { rem, style, vertical, verticallySpaced } from '../typestyle-exports'
 import { noop } from 'ramda-adjunct/'
-import { storageGet, StorageSet } from './StorageSet'
-import { categories } from '../models/Category'
-import type { TaskModel } from '../models/Task'
 import Component from '@reach/component-component'
 import { createNumberValue } from 'react-values'
 
@@ -58,96 +52,5 @@ export function ModalState({ trigger, children }: ModalProps) {
         </Component>
       )}
     />
-  )
-}
-
-export function EditTaskDialog() {
-  return (
-    <Consumer>
-      {({
-        onDismiss,
-        isOpen,
-        title,
-        category,
-        onTitleChange,
-        onCategoryChange,
-        onOk,
-      }) => (
-        <Dialog
-          className={style(verticallySpaced(rem(1)))}
-          onDismiss={onDismiss}
-          isOpen={isOpen}
-        >
-          <h3>Edit Task </h3>
-          <div className={style(vertical)}>
-            <input type={'text'} value={title} onChange={onTitleChange} />
-          </div>
-          <div className={style(vertical)}>
-            <select value={category} onChange={onCategoryChange}>
-              {categories.map(category => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <button onClick={onOk}>Ok</button>
-          </div>
-        </Dialog>
-      )}
-    </Consumer>
-  )
-}
-
-export function showEditTaskDialog(task: TaskModel, render: Function) {
-  return (
-    <Consumer>
-      {({ startEditingTask }) => render(startEditingTask(task))}
-    </Consumer>
-  )
-}
-
-export function EditTaskDialogStateProvider({ children }: { children: any }) {
-  const stateName = 'editTaskState'
-  const defaultState = { isOpen: false, task: {}, title: '', category: '' }
-  return (
-    <Component getInitialState={() => storageGet(stateName, defaultState)}>
-      {({ state, setState }) =>
-        renderWithCollections(({ updateTask }) => {
-          const { task, isOpen, title, category } = state
-          const onDismiss = () => setState({ isOpen: false })
-          const startEditingTask = task => () =>
-            setState({
-              isOpen: true,
-              task,
-              title: task.title,
-              category: task.category,
-            })
-          const onOk = () => {
-            updateTask({ title, category }, task)
-            onDismiss()
-          }
-          const onTitleChange = e => setState({ title: e.target.value })
-          const onCategoryChange = e => setState({ category: e.target.value })
-          const childProps = {
-            onDismiss,
-            isOpen,
-            title,
-            onTitleChange,
-            onCategoryChange,
-            onOk,
-            startEditingTask,
-            category,
-          }
-          return (
-            <Fragment>
-              <StorageSet name={stateName} value={state} />
-              <Provider value={childProps}>{children}</Provider>
-            </Fragment>
-          )
-        })
-      }
-    </Component>
   )
 }
