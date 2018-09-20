f8c241f87d42e0c8d086921e350de40b2177240d
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 00:41:24 +0530

    firebase, state & streams

--- src/components/Value.js
+++ src/components/Value.js
@@ -3,29 +3,6 @@ import { compose, prop } from 'ramda'
 import { toRenderProps, withProps, withState } from 'recompose'
 import { p } from '../promise'
 
-// export class Value extends React.Component {
-//   set = value => {
-//     return p((resolve, reject) => {
-//       try {
-//         const newState = ifElse(
-//           isFunction,
-//           over(lensProp('value'), __),
-//           objOf('value'),
-//         )(value)
-//         this.setState(newState, () => resolve(this.state))
-//       } catch (e) {
-//         reject(e)
-//       }
-//     })
-//   }
-//
-//   state = { value: this.props.value, set: this.set }
-//
-//   render() {
-//     return this.props.children(this.state)
-//   }
-// }
-
 export const Value = toRenderProps(
   compose(
     withState('value', 'set', prop('value')),
