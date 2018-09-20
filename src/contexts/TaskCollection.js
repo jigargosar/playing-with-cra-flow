b2771125123c89d56332316b95fcd3c8a76d31ab
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 01:58:19 +0530

    firebase, state & streams

--- src/contexts/TaskCollection.js
+++ src/contexts/TaskCollection.js
@@ -4,23 +4,14 @@ import { compose, map, pick } from 'ramda'
 import { defaultProps, toRenderProps, withHandlers } from 'recompose'
 import { withValue } from '../components/Value'
 
-// const findIndexById = (id, list) => list.findIndex(m => m.id === id)
-// const taskUpdater = (changes, task) => ({ tasks }) => {
-//   const idx = findIndexById(task.id, tasks)
-//   console.assert(idx !== -1)
-//   return { tasks: update(idx, { ...tasks[idx], ...changes }, tasks) }
-// }
-
 const updateTask = tasks => (changes, { id }) =>
   tasks.set(
-    map(t => {
-      // if (t.id === id) {
-      //   debugger
-      // }
-      return t.id === id
-        ? { ...t, ...pick(['title', 'category', 'done'])(changes) }
-        : t
-    }),
+    map(
+      t =>
+        t.id === id
+          ? { ...t, ...pick(['title', 'category', 'done'])(changes) }
+          : t,
+    ),
   )
 export const TaskCollection = toRenderProps(
   compose(
@@ -28,31 +19,11 @@ export const TaskCollection = toRenderProps(
     withValue,
     withHandlers({
       updateTask,
-      toggleDone: tasks => task => {
-        return updateTask(tasks)({ done: !task.done }, task)
-      },
+      toggleDone: tasks => task =>
+        updateTask(tasks)({ done: !task.done }, task),
     }),
   ),
 )
-
-// export const TaskCollection = adopt(
-//   {
-//     tasks: <Value initial={loadOrGenerateTasks} onChange={saveTasks} />,
-//   },
-//   ({ tasks }) => {
-//     return {
-//       value: tasks.value,
-//       updateTask: (changes, { id }) =>
-//         tasks.set(
-//           map(t => {
-//             return t.id === id
-//               ? { ...t, ...pick(['title', 'category'])(changes) }
-//               : t
-//           }),
-//         ),
-//     }
-//   },
-// )
 
 const Context = React.createContext()
 
