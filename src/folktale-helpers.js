0d16afc1c5d3ab2caa16dd81e8a1ade09c4a1c05
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 00:12:48 +0530

    firebase, state & streams

--- src/folktale-helpers.js
+++ src/folktale-helpers.js
@@ -1,19 +1,5 @@
 import { nullableToMaybe } from 'folktale/conversions'
-import Maybe from 'folktale/maybe'
-import { __, clamp, compose, curry, find, map, propEq } from 'ramda'
-
-export const atIndex = curry((idx, list) => nullableToMaybe(list[idx]))
-
-export const atClampedIndex = curry((idx, list) => {
-  return compose(
-    map(atIndex(__)),
-    clampIndex(idx),
-  )(list)
-})
-
-export const clampIndex = curry((idx, list) => {
-  return list.length === 0 ? Maybe.Nothing : clamp(0, list.length - 1, idx)
-})
+import { compose, curry, find, propEq } from 'ramda'
 
 export const findById = curry((id, c) =>
   compose(
