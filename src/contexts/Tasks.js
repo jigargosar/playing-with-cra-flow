65bc7f5d056bd72d8ffd1cb6506a1a3d726aa73f
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 01:03:42 +0530

    firebase, state & streams

--- src/contexts/Tasks.js
+++ src/contexts/Tasks.js
@@ -1,7 +1,24 @@
 import * as React from 'react'
 import { adopt } from 'react-adopt'
 import { Value } from '../components/Value'
+import { loadOrGenerateTasks } from '../models/Task'
+import { map, pick } from 'ramda'
 
-export const Tasks = adopt({
-  tasks: <Value value={1} />,
-})
+export const Tasks = adopt(
+  {
+    tasks: <Value value={loadOrGenerateTasks} />,
+  },
+  ({ tasks }) => {
+    return {
+      value: tasks.value,
+      updateTask: (changes, { id }) =>
+        tasks.set(
+          map(t => {
+            return t.id === id
+              ? { ...t, ...pick(['title', 'category'])(changes) }
+              : t
+          }),
+        ),
+    }
+  },
+)
