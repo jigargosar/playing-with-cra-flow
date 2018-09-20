540b163c0f502a087ec1bb2b76ebdefa5781bcff
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 00:24:00 +0530

    firebase, state & streams

--- src/components/Value.js
+++ src/components/Value.js
@@ -1,27 +1,28 @@
 import * as React from 'react'
-import { p } from '../promise'
-import { __, ifElse, lensProp, objOf, over } from 'ramda'
-import { isFunction } from 'ramda-adjunct'
+import { compose, identity, prop } from 'ramda'
+import { withState } from 'recompose'
 
-export class Value extends React.Component {
-  set = value => {
-    return p((resolve, reject) => {
-      try {
-        const newState = ifElse(
-          isFunction,
-          over(lensProp('value'), __),
-          objOf('value'),
-        )(value)
-        this.setState(newState, () => resolve(this.state))
-      } catch (e) {
-        reject(e)
-      }
-    })
-  }
+// export class Value extends React.Component {
+//   set = value => {
+//     return p((resolve, reject) => {
+//       try {
+//         const newState = ifElse(
+//           isFunction,
+//           over(lensProp('value'), __),
+//           objOf('value'),
+//         )(value)
+//         this.setState(newState, () => resolve(this.state))
+//       } catch (e) {
+//         reject(e)
+//       }
+//     })
+//   }
+//
+//   state = { value: this.props.value, set: this.set }
+//
+//   render() {
+//     return this.props.children(this.state)
+//   }
+// }
 
-  state = { value: this.props.value, set: this.set }
-
-  render() {
-    return this.props.children(this.state)
-  }
-}
+export const Value = compose(withState('value', 'set', prop('value')))(identity)
