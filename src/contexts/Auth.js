01abc465f81af2085801c47ea1098c8f9d7a5ec3
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 18:35:52 +0530

    trying out proppy

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -1,6 +1,7 @@
 import { getOrCreateFirebaseApp, signIn, signOut } from '../lib/fire'
 import { compose as proppy, emit, withHandlers, withProps } from 'proppy'
 import { attach } from 'proppy-react'
+import { toRenderProps } from 'recompose'
 
 const AuthFactory = proppy(
   withProps({ status: 'unknown', user: null, signIn, signOut }),
@@ -15,8 +16,5 @@ const AuthFactory = proppy(
     match: ({ status, user }) => matcher => matcher[status](user),
   }),
 )
-const RenderProps = ({ children, ...pp }) => children(pp)
-
-const toRenderProps = hoc => hoc(RenderProps)
 
 export const Auth = toRenderProps(attach(AuthFactory))
