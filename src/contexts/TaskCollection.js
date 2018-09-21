7bf67820cd5c268e430ad943b821cb41db3c3f93
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 16:37:21 +0530

    trying out proppy

--- src/contexts/TaskCollection.js
+++ src/contexts/TaskCollection.js
@@ -1,41 +1,3 @@
-// import * as React from 'react'
-// import { generateTask, loadOrGenerateTasks, saveTasks } from '../models/Task'
-// import { append, compose, map, pick, propEq, when } from 'ramda'
-// import { defaultProps, toRenderProps, withHandlers } from 'recompose'
-// import { withValue } from '../components/Value'
-// import { mergeRight } from '../ramda-exports'
-//
-// const pickUserChanges = pick(['title', 'category', 'done'])
-//
-// const updateTask = tasks => (changes, { id }) =>
-//   tasks.set(map(when(propEq('id', id))(mergeRight(pickUserChanges(changes)))))
-//
-// export const TaskCollection = toRenderProps(
-//   compose(
-//     defaultProps({ initial: loadOrGenerateTasks, onChange: saveTasks }),
-//     withValue,
-//     withHandlers({
-//       update: updateTask,
-//       toggleDone: tasks => task =>
-//         updateTask(tasks)({ done: !task.done }, task),
-//       add: tasks => () => {
-//         const task = { ...generateTask(), category: 'InBasket' }
-//         tasks.set(append(mergeRight(task)))
-//       },
-//     }),
-//   ),
-// )
-//
-// const Context = React.createContext()
-//
-// export const TaskCollectionProvider = ({ children }) => (
-//   <TaskCollection
-//     children={value => <Context.Provider value={value} children={children} />}
-//   />
-// )
-//
-// export const TaskCollectionConsumer = Context.Consumer
-
 import { generateTask, loadOrGenerateTasks, saveTasks } from '../models/Task'
 import { compose as proppy, onChange, withHandlers, withState } from 'proppy'
 import { append, compose, map, pick, prop, propEq, when } from 'ramda'
@@ -45,19 +7,19 @@ import { attach } from 'proppy-react'
 
 const pickUserChanges = pick(['title', 'category', 'done'])
 
-const updateTask = tasks => (changes, { id }) =>
-  tasks.set(
-    map(when(propEq('id', id))(mergeRight(pickUserChanges(changes))))(
-      tasks.value,
-    ),
-  )
-
 export const TaskCollection = proppy(
   withState('value', 'set', loadOrGenerateTasks()),
   withHandlers({ over: ({ value, set }) => fn => set(fn(value)) }),
   withHandlers({
-    update: updateTask,
-    toggleDone: tasks => task => updateTask(tasks)({ done: !task.done }, task),
+    update: tasks => (changes, { id }) =>
+      tasks.set(
+        map(when(propEq('id', id))(mergeRight(pickUserChanges(changes))))(
+          tasks.value,
+        ),
+      ),
+  }),
+  withHandlers({
+    toggleDone: ({ update }) => task => update({ done: !task.done }, task),
     add: tasks => () => {
       const task = { ...generateTask(), category: 'InBasket' }
       tasks.over(append(task))
