db34dce090212b0374fe242e8f7cbc1136fc8df5
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 15:41:41 +0530

    trying out proppy

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
 // @flow
 import './index.css'
 import App from './App'
-import hydux from 'hydux'
 // import withPersist from 'hydux/lib/enhancers/persist'
-import withReact, { React } from 'hydux-react'
+import { React } from 'hydux-react'
 import { setupGlobalStyles } from './styles'
 import { hmrSetup } from './hmr'
-
-setupGlobalStyles()
-
-export default withReact(document.getElementById('root'), {
-  useComponent: true,
-})(hydux)({
-  init: () => ({
-    count: 1,
-  }),
-  actions: {
-    down: () => state => ({ count: state.count - 1 }),
-    up: () => state => ({ count: state.count + 1 }),
-  },
-  view: (state, actions) => <App state={state} actions={actions} />,
-})
+import registerServiceWorker from './registerServiceWorker'
+import { renderRootApp } from './react-helpers'
+import { forceRenderStyles } from 'typestyle'
 
 // setupGlobalStyles()
-// renderRootApp(App)
-//   .then(forceRenderStyles)
-//   .catch(console.error)
 //
-// registerServiceWorker()
+// export default withReact(document.getElementById('root'), {
+//   useComponent: true,
+// })(hydux)({
+//   init: () => ({
+//     count: 1,
+//   }),
+//   actions: {
+//     down: () => state => ({ count: state.count - 1 }),
+//     up: () => state => ({ count: state.count + 1 }),
+//   },
+//   view: (state, actions) => <App state={state} actions={actions} />,
+// })
+//
+setupGlobalStyles()
+renderRootApp(App)
+  .then(forceRenderStyles)
+  .catch(console.error)
+
+registerServiceWorker()
 
 if (process.env.NODE_ENV === 'development') {
   hmrSetup(module)
