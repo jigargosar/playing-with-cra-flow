5e1308f0c5fd1f1740d89463c3c72c64f4274249
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-21 19:28:35 +0530

    direct firestore updates

--- src/contexts/FireTaskCollection.js
+++ src/contexts/FireTaskCollection.js
@@ -1,44 +1,25 @@
-import {
-  filterTasks,
-  generateTask,
-  loadOrGenerateTasks,
-  saveTasks,
-} from '../models/Task'
-import {
-  compose as proppy,
-  map as mapProps,
-  onChange,
-  withHandlers,
-  withState,
-} from 'proppy'
-import { append, map, pick, propEq, tap, when } from 'ramda'
-import { mergeRight } from '../ramda-exports'
+import { filterTasks, generateTask } from '../models/Task'
+import { compose as proppy, withHandlers, withProps } from 'proppy'
+import { pick } from 'ramda'
 import * as React from 'react'
 import { attach } from 'proppy-react'
 
-const pickUserChanges = pick(['title', 'category', 'done'])
+export const pickUserChanges = pick(['title', 'category', 'done'])
 
 export const TaskCollection = proppy(
-  withState('_tasks', 'setTasks', loadOrGenerateTasks()),
+  withProps({ allTasks: [] }),
   withHandlers({
-    updateTask: ({ _tasks, setTasks }) => (changes, { id }) =>
-      setTasks(
-        map(when(propEq('id', id))(mergeRight(pickUserChanges(changes))))(
-          _tasks,
-        ),
-      ),
+    updateTask: () => () => {},
   }),
   withHandlers({
     toggleDone: ({ updateTask }) => task =>
       updateTask({ done: !task.done }, task),
-    add: ({ setTasks, _tasks }) => () => {
+    add: () => () => {
       const task = { ...generateTask(), category: 'InBasket' }
-      setTasks(append(task, _tasks))
+      return task
     },
-    filterTasks: ({ _tasks }) => pred => filterTasks(pred)(_tasks),
+    filterTasks: ({ allTasks }) => pred => filterTasks(pred)(allTasks),
   }),
-  onChange('_tasks', ({ _tasks }) => ({ _tasks: tap(saveTasks)(_tasks) })),
-  mapProps(({ _tasks, ...other }) => ({ allTasks: _tasks, ...other })),
 )
 
 const Context = React.createContext()
