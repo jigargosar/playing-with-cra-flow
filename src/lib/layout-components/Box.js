0c4e6941c4dfb258be1157a90ac50a8fecd699cb
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-22 23:11:57 +0530

    using layout components

--- src/lib/layout-components/Box.js
+++ src/lib/layout-components/Box.js
@@ -6,6 +6,7 @@ import {
   verticallySpaced,
 } from 'csstips'
 import { createComponentFromMapper } from './createComponentFromMapper'
+import { defaultProps } from 'recompose'
 
 export const HBox = createComponentFromMapper(
   ({ className, spacing = null, ...otherProps }) => ({
@@ -17,6 +18,11 @@ export const HBox = createComponentFromMapper(
   }),
 )
 
+const defaultSpacing = spacing => defaultProps({ spacing })
+
+export const HBox8 = defaultSpacing(8)(HBox)
+export const HBox16 = defaultSpacing(8)(HBox)
+
 export const VBox = createComponentFromMapper(
   ({ className, spacing = null, ...otherProps }) => ({
     className: classes(
@@ -26,3 +32,6 @@ export const VBox = createComponentFromMapper(
     ...otherProps,
   }),
 )
+
+export const VBox8 = defaultSpacing(8)(VBox)
+export const VBox16 = defaultSpacing(8)(VBox)
