c655c3c924d61e8f83de887f49494ccf7ddec398
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-22 23:07:08 +0530

    using layout components

--- src/lib/layout-components/HBox.js
+++ src/lib/layout-components/HBox.js
@@ -1,15 +1,1 @@
-import { classes, style } from 'typestyle'
-import { horizontal, horizontallySpaced } from 'csstips'
-import { createComponentFromMapper } from './createComponentFromMapper'
-
-const HBox = createComponentFromMapper(
-  ({ className, spacing = null, ...otherProps }) => ({
-    className: classes(
-      className,
-      style(horizontal, spacing && horizontallySpaced(spacing)),
-    ),
-    ...otherProps,
-  }),
-)
-
-export default HBox
+export { HBox as default } from './Box'
--- src/lib/layout-components/VBox.js
+++ src/lib/layout-components/VBox.js
@@ -1,13 +1,1 @@
-import { classes, style } from 'typestyle'
-import { vertical, verticallySpaced } from 'csstips'
-import { createComponentFromMapper } from './createComponentFromMapper'
-
-export default createComponentFromMapper(
-  ({ className, spacing = null, ...otherProps }) => ({
-    className: classes(
-      style(vertical, spacing && verticallySpaced(spacing)),
-      className,
-    ),
-    ...otherProps,
-  }),
-)
+export { VBox as default } from './Box'
