2f9d9cf884726c013919e768f13cfef01b262c52
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-22 23:03:32 +0530

    using layout components

--- src/lib/layout-components/VBox.js
+++ src/lib/layout-components/VBox.js
@@ -1,12 +1,11 @@
 import { classes, style } from 'typestyle'
 import { vertical, verticallySpaced } from 'csstips'
-import { rem } from 'csx'
 import { createComponentFromMapper } from './createComponentFromMapper'
 
 export default createComponentFromMapper(
   ({ className, spacing = null, ...otherProps }) => ({
     className: classes(
-      style(vertical, spacing && verticallySpaced(rem(spacing))),
+      style(vertical, spacing && verticallySpaced(spacing)),
       className,
     ),
     ...otherProps,
