1206852d287ac484d54febdb19832af04285359f
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-22 20:52:06 +0530

    storybook HBox

--- src/stories/index.js
+++ src/stories/index.js
@@ -8,17 +8,17 @@ import { bg } from '../styles'
 import { gray } from '../colors'
 import { mapIndexed } from '../ramda-exports'
 
-const Block = props => (
+const DemoBlock = props => (
   <div
     className={style(padding('1rem'), { border: '1px solid black' })}
     {...props}
   />
 )
 
-const demoContent = mapIndexed((text, idx) => <Block key={idx}>{text}</Block>, [
-  'foo',
-  'bar',
-])
+const demoContent = mapIndexed(
+  (text, idx) => <DemoBlock key={idx}>{text}</DemoBlock>,
+  ['foo', 'bar'],
+)
 
 const DemoContainer = props => (
   <div className={style(bg(gray(0.1)))} {...props} />
@@ -27,14 +27,12 @@ const DemoContainer = props => (
 storiesOf('HBox', module)
   //
   .add('without spacing', () => (
-    <div>
-      <DemoContainer>
-        <HBox>{demoContent}</HBox>
-      </DemoContainer>
-    </div>
+    <DemoContainer>
+      <HBox>{demoContent}</HBox>
+    </DemoContainer>
   ))
   .add('with spacing', () => (
-    <div className={style(bg(gray(0.1)))}>
+    <DemoContainer>
       <HBox spacing={'1rem'}>{demoContent}</HBox>
-    </div>
+    </DemoContainer>
   ))
