ba71c2b82f3e70aea65b9e94d7926fb79c1e3ffc
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-22 21:46:31 +0530

    storybook HBox

--- src/components/HBox.js
+++ src/components/HBox.js
@@ -2,17 +2,6 @@ import React from 'react'
 import { classes, style } from 'typestyle'
 import { horizontal, horizontallySpaced } from 'csstips'
 import { rem } from 'csx'
-import { mapProps, proper } from '../lib/proper'
-
-export const P = proper(
-  mapProps(({ className, spacing = 0, ...op }) => ({
-    className: classes(
-      style(horizontal, horizontallySpaced(rem(spacing))),
-      className,
-    ),
-    ...op,
-  })),
-)
 
 const mapProps = ({ className, spacing = 0, ...otherProps }) => ({
   className: classes(
